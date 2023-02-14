dce8095e7ed65d9cc8d51ab3a1934e9e6d70edfd
thomybanez <thomybanez@yahoo.com>
2023-02-14 08:49:26 +0800

    "Sir Carl Pulled Files"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
-// import SlackInterface from './pages/SlackInterface.js'
-// import SignUp from './js-component/SignUp';
 import ProjectMain from './ProjectMain';
-import SlackInterface from './pages/SlackInterface.js'
+// import SlackInterface from './pages/SlackInterface.js'
 // import SignUp from './js-component/SignUp';
 
 function App() {
   return (
     <>
         <ProjectMain />
-        {/* <SignUp /> */}
-        <SlackInterface />
     </>
   );
 }
