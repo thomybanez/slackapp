c3c98fcb1e89aab0c3cf245676ca47a7cf6164f1
thomybanez <thomybanez@yahoo.com>
2023-02-16 19:46:12 +0800

    Updated

--- src/js-component/LogIn.js
+++ src/js-component/LogIn.js
@@ -1,50 +1,50 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
-var email = document.forms['form']['email'];
-var password = document.forms['form']['password'];
+// var email = document.forms['form']['email'];
+// var password = document.forms['form']['password'];
     
-var email_error = document.getElementById('email_error');
-var pass_error = document.getElementById('pass_error');
+// var email_error = document.getElementById('email_error');
+// var pass_error = document.getElementById('pass_error');
     
-email.addEventListener('textInput', email_Verify);
-password.addEventListener('textInput', pass_Verify);
+// email.addEventListener('textInput', email_Verify);
+// password.addEventListener('textInput', pass_Verify);
     
-function validated(){
-  if (email.value.length < 9) {
-    email.style.border = "1px solid red";
-    email_error.style.display = "block";
-    email.focus();
-  return false;
-  }
+// function validated(){
+//   if (email.value.length < 9) {
+//     email.style.border = "1px solid red";
+//     email_error.style.display = "block";
+//     email.focus();
+//   return false;
+//   }
 
-  if (password.value.length < 6) {
-    password.style.border = "1px solid red";
-    pass_error.style.display = "block";
-    password.focus();
-  return false;
-  }
+//   if (password.value.length < 6) {
+//     password.style.border = "1px solid red";
+//     pass_error.style.display = "block";
+//     password.focus();
+//   return false;
+//   }
     
-  }
+//   }
 
-function email_Verify(){
-  if (email.value.length >= 8) {
-    email.style.border = "1px solid silver";
-    email_error.style.display = "none";
-   return true;
-  }
-}
+// function email_Verify(){
+//   if (email.value.length >= 8) {
+//     email.style.border = "1px solid silver";
+//     email_error.style.display = "none";
+//    return true;
+//   }
+// }
     
-function pass_Verify(){
-  if (password.value.length >= 5) {
-  password.style.border = "1px solid silver";
-  pass_error.style.display = "none";
-  return true;
-}
-}
+// function pass_Verify(){
+//   if (password.value.length >= 5) {
+//   password.style.border = "1px solid silver";
+//   pass_error.style.display = "none";
+//   return true;
+// }
+// }
